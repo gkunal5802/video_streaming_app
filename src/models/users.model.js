edd768a424638cded07332e0954af0be549b9f48
kunal garg <kunalg5802@gmail.com>
2024-04-05 21:18:45 +0530

    modelling and jwt

---
+++ src/models/users.model.js
@@ -0,0 +1,89 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      lowercase: true,
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+      index: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: [true, "username is required"],
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    watchHistory: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+    avatar: {
+      type: String, // cloudinary url,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// PRE middleware is used to perform actions just before some action like saving the document here.
+userSchema.pre("save", async function (next) {
+  // it is necessary to check whether password was changed before saving/updating the document or not. If any other field is updated then it will directly move to next middleware.
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+export const User = mongoose.model("User", userSchema);
